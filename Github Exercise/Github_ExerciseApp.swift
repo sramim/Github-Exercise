9908e65e1e00f1596cebac420b055622c4ff6a27
Kaneis Zontanos <ramim@bc.edu>
2023-01-22 14:54:05 -0500

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Kaneis Zontanos on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
